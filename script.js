7ce20fcef7891349bdf2ce5e883fe2969e1e54d1
lizxone9 <lizxone9@gmail.com>
2026-01-28 15:57:05 +0500

    Complete redesign of Portfolio: AI & Product focus

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["UX/UI Designer", "Frontend Developer", "Mobile Developer"],
+const typed = new Typed('.multiple-text', {
+    strings: [
+        'Product Engineer', 
+        'AI Solutions Architect', 
+        'Full-Stack Developer', 
+        'Founder of Mirumitech'
+    ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
